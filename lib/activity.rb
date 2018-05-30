554a612ef7a516fdf3652fd4927d0ddcc9ffe1d0
Erik Schneider <erikschneider2211@gmail.com>
2018-05-30 10:37:59 -0600

    Initial Commit

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,28 @@
 class Activity
+  attr_reader  :cost,
+               :name,
+               :participant, 
+               :activity,
+               :players
+  
+  def initialize(name, participant, cost)
+    @activity = activity 
+    @name = name
+    @participant = participant 
+    @cost = 0
+    @players = {}
+  end
+  
+  def hockey
+    "Hockey"
+  end 
+  
+  def participant
+    @players = {
+      "Bob" => 50,
+      "Fred" => 75,
+      "Alice" => 100
+    }
+  end 
+    
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,19 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  
+  def test_it_exists
+    activity = Activity.new("Hockey", "Bob", 50)
+    assert_instance_of Activity, activity 
+  end 
+  
+  def test_the_activity_is_hockey
+    activity = Activity.new("Hockey", "Bob", 50) 
+    assert_equal "Hockey", activity.hockey 
+  end 
+  
+  def test_the_participants
+    activity = Activity.new("Hockey", "Bob", 50)
+    assert_equal @players[0], activity.participant 
+  end 
 end
